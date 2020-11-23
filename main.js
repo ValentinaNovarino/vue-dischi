e05b9de2d537412db03154a71c9b7d243d3d9e14
ValentinaNovarino <valenovarino@gmail.com>
2020-11-23 16:34:33 +0100

    stampato cards

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 var app = new Vue ( {
     el: '#root',
     data: {
-
+        discs: []
     },
     mounted() {
-        
+        var self = this;
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then(function(cd) {
+            self.discs = cd.data.response;
+            console.log(self.discs);
+        })
     }
 })
